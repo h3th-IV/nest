1c843f1b487301c25e211c6da16b5d9c699bab1f
B2N10X30 <samuelbonux10@gmail.com>
2023-12-20 16:01:09 +0100

    Net and the good stuff

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Home Page")
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -19,38 +23,37 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %v,\n Welcome to hello page", name)
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Home Page")
-}
 func main() {
 	//intialize the get request to host
-	resp, err := http.Get("https://github.com/jim-nnamdi?tab=repositories")
-	if err != nil {
-		panic(err)
-	}
-	//close the body of response
-	defer resp.Body.Close()
+	// resp, err := http.Get("https://github.com/jim-nnamdi?tab=repositories")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// //close the body of response
+	// defer resp.Body.Close()
 
-	//scan the resp and print output
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	// //scan the resp and print output
+	// scanner := bufio.NewScanner(resp.Body)
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// if err := scanner.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
+	fileServer := http.FileServer(http.Dir("./static"))
 	//now the server
 	//create new ServeMultiplexer
 	mux := http.NewServeMux()
 
 	//the new mux is used to route requst
+	http.Handle("/", fileServer)
 	mux.HandleFunc("/form", formHandler)
 	mux.HandleFunc("/", homeHandler)
 
 	fmt.Println("Listeniing on :8090")
-	err = http.ListenAndServe("0.0.0.0:8090", mux)
+	err := http.ListenAndServe(":8090", mux)
 	if err != nil {
 		panic(err)
 	}
