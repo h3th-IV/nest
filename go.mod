a739fb49b06cf3c0b84b9b647793626cfdb8b72b
B2N10X30 <samuelbonux10@gmail.com>
2023-12-22 07:18:19 +0100

    route parameters

--- go.mod
+++ go.mod
@@ -7,4 +7,4 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -10,6 +10,58 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func main() {
+	//intialize the get request to host
+	// resp, err := http.Get("https://github.com/jim-nnamdi?tab=repositories")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// //close the body of response
+	// defer resp.Body.Close()
+
+	// //scan the resp and print output
+	// scanner := bufio.NewScanner(resp.Body)
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
+
+	// if err := scanner.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	//now the server
+	//create new ServeMultiplexer
+	// router := http.NewServeMux()
+
+	//create a new router
+	router := mux.NewRouter()
+
+	//the new mux is used to route requst
+	router.Handle("/", fileServer)
+	router.HandleFunc("/form", formHandler)
+	router.HandleFunc("/hello", helloHandler)
+
+	//route parameters used to capture dynamic values from incomin request
+	//this is done with mux.Vars
+	router.HandleFunc("/user/{id:[0-9]+}", userHandler)
+	router.HandleFunc("/topics/{category}/{id:[0-9]+}", topicHandler)
+
+	fmt.Println("Listening on :8090")
+	err := http.ListenAndServe(":8090", router)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func topicHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	category := vars["category"]
+	topicID := vars["id"]
+
+	fmt.Fprintf(w, "Welcome to the %v page, you are under %v ", category, topicID)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Our Hello Page")
 }
@@ -49,47 +101,4 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 	Var := mux.Vars(r)
 	userID := Var["id"]
 	fmt.Fprintf(w, "Welcome user: %v", userID)
-}
-
-func main() {
-	//intialize the get request to host
-	// resp, err := http.Get("https://github.com/jim-nnamdi?tab=repositories")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// //close the body of response
-	// defer resp.Body.Close()
-
-	// //scan the resp and print output
-	// scanner := bufio.NewScanner(resp.Body)
-	// for scanner.Scan() {
-	// 	fmt.Println(scanner.Text())
-	// }
-
-	// if err := scanner.Err(); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	fileServer := http.FileServer(http.Dir("./static"))
-	//now the server
-	//create new ServeMultiplexer
-	// router := http.NewServeMux()
-
-	//create a new router
-	router := mux.NewRouter()
-
-	//the new mux is used to route requst
-	router.Handle("/", fileServer)
-	router.HandleFunc("/form", formHandler)
-	router.HandleFunc("/hello", helloHandler)
-
-	//route parameters used to capture dynamic values from incomin request
-	//this is done with mux.Vars
-	router.HandleFunc("/user/{id:[0-9]+}", userHandler)
-
-	fmt.Println("Listening on :8090")
-	err := http.ListenAndServe(":8090", router)
-	if err != nil {
-		panic(err)
-	}
 }
