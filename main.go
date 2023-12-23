d6edd6d8a9ec59e066e1a175afc9835b765e51ac
B2N10X30 <samuelbonux10@gmail.com>
2023-12-24 00:51:28 +0100

    middleware it's called

--- main.go
+++ main.go
@@ -29,14 +29,18 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
+	//serve static files
 	fileServer := http.FileServer(http.Dir("./static"))
 	//now the server
-	//create new ServeMultiplexer
+	//create new ServeMultiplexer(or better put router)
 	// router := http.NewServeMux()
 
 	//create a new router
 	router := mux.NewRouter()
+	router.Use(loggingMiddleware)
 
+	//create file sever with mux
+	// router.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
 	//the new mux is used to route requst
 	router.Handle("/", fileServer).Methods("GET") //we can use the Methods method to add HTTP method consraints.
 	router.HandleFunc("/form", formHandler).Methods("")
@@ -44,14 +48,13 @@ func main() {
 
 	//route parameters used to capture dynamic values from incomin request
 	//this is done with mux.Vars
-	// router.HandleFunc("/user/{id:[0-9]+}", userHandler)
-	// router.HandleFunc("/topics/{category}/{id:[0-9]+}", domainHandler)
-
 	//subrouters are used to organise and group related routes under a common url
-	userRouter := router.PathPrefix("/users").Subrouter()
+
+	userRouter := router.PathPrefix("/login").Subrouter()
 	userRouter.HandleFunc("/", userHandler).Methods("GET")
+	userRouter.HandleFunc("/account/{password}", loginHAndler)
 	userRouter.HandleFunc("/profile", userProfileHandler).Methods("GET")
-	userRouter.HandleFunc("/{username:[a-zA-z0-9]+}", singleUserHandler).Methods("GET")
+	userRouter.HandleFunc("/{username:[a-zA-z0-9!@#$%^&*()-_+]+}", singleUserHandler).Methods("GET")
 
 	domainRouter := router.PathPrefix("/domains").Subrouter()
 	domainRouter.HandleFunc("/", domainHandler).Methods("GET")
@@ -62,6 +65,53 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome pls go the Login page -> /login/password \ncreate an account -> /form")
+}
+
+func loginHAndler(w http.ResponseWriter, r *http.Request) {
+	var username string
+	Vars := mux.Vars(r)
+	passowrd := Vars["password"]
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	dB, err := databses.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer databses.Close()
+
+	query := `SELECT username FROM users WHERE password = ?`
+	stmt, err := dB.Prepare(query)
+	if err != nil {
+		log.Print(err)
+	}
+	defer stmt.Close()
+	useRow, err := stmt.Query(passowrd)
+	if err != nil {
+		http.Error(w, "Internal Server error", http.StatusInternalServerError)
+	}
+	defer useRow.Close()
+	//check if user with password exist
+	if useRow.Next() {
+		useRow.Scan(&username)
+		fmt.Fprintf(w, "Welcome %v, have successfully login", username)
+	} else {
+		http.Error(w, "User Account not Found", http.StatusUnauthorized)
+	}
+}
+
+// middleware
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request URL: %s\n", r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func userProfileHandler(w http.ResponseWriter, r *http.Request) {
@@ -113,14 +163,11 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer databses.Close()
 	//use prepatred statement
 	query := `INSERT INTO users(username, password) VALUES(?, ?)`
 	_, err = dB.Query(query, name, password)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome new user")
 }
